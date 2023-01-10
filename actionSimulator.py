0e748981eb7e4593f224fb84eecc74c23e8886df
ycTestGitter <121527799+ycTestGitter@users.noreply.github.com>
2023-01-10 14:53:57 +0800

    fix convert to exe not detect the config file.

--- actionSimulator.py
+++ actionSimulator.py
@@ -228,7 +228,16 @@ def testCase(mode):
         simuUserType('tamp')
         time.sleep(0.5)
         recorder.replay(clearDesk=False, keyFlg=False)
-
+    elif mode == 4: 
+        import os
+        import sys
+        config_name = 'myapp.cfg'
+        # determine if application is a script file or frozen exe
+        if getattr(sys, 'frozen', False):
+            application_path = os.path.dirname(sys.executable)
+        elif __file__:
+            application_path = os.path.dirname(__file__)
+        config_path = os.path.join(application_path, config_name)
     else:
         recorder = actionSimulator()
         recorder.resetActionRcd()
